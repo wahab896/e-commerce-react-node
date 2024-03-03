2adae8fcfe86fc7d726d8b0f18fee28b15a0fa68
Wahab Memon <memonwahab19@gmail.com>
2024-03-03 17:44:31 +0530

    Deleted extra files

--- backend/data.js
+++
@@ -1,4 +0,0 @@
-export default [
-  { name: "jason", department: "tech", id: 1 },
-  { name: "abcd", department: "tech", id: 2 },
-];
--- backend/users.js
+++
@@ -1,6 +0,0 @@
-import bcrypt from "bcrypt";
-
-export default [
-  { userId: 123, password: bcrypt.hashSync("123456", 10), userName: "test" },
-  { userId: 123, password: bcrypt.hashSync("abcd123", 10), userName: "test2" }
-];
