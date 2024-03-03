9eca4289f37bd5e70b7fca9a974ecf477855e0ec
Wahab Memon <memonwahab19@gmail.com>
2024-03-03 17:55:22 +0530

    Models added

---
+++ backend/models/userModel.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+// Match user entered password to hashed password in database
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPass.this.password);
+};
+
+// Encrypt password using bcrypt
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
