4a310c351276a905c627b0eb2f17253ea92d90da
Wahab Memon <memonwahab19@gmail.com>
2024-03-09 19:49:38 +0530

    Added toastify

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <>
+      <ToastContainer />
       <Header />
       <main>
-        <Outlet/>
+        <Outlet />
       </main>
       <Footer />
     </>
