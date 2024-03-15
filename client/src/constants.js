df349c7725940a802ecdc0cf3af82b0766ca2f34
Wahab Memon <memonwahab19@gmail.com>
2024-03-15 11:54:23 +0530

    redux added

---
+++ client/src/constants.js
@@ -0,0 +1,5 @@
+export const BASE_URL = "";
+export const USERS_URL = "/api/users";
+export const PRODUCTS_URL = "/api/products";
+export const ORDERS_URL = "/api/users";
+export const PAYPAL_URL = "/api/config/paypal";
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,6 +7,8 @@ import {
   createRoutesFromElements,
 } from "react-router-dom";
 import "./index.css";
+import store from "./store.js";
+import { Provider } from "react-redux";
 import App from "./App.jsx";
 import ProfileScreen from "./screens/ProfileScreen.jsx";
 import HomeScreen from "./screens/HomeScreen.jsx";
@@ -26,6 +28,8 @@ const router = createBrowserRouter(
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
